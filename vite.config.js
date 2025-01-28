37c727bbccabd973cbb65dbc0614df3d8e6b3f6a
SPallazhco <sergiopall@hotmail.com>
2025-01-28 18:57:28 -0500

    Errores de rutas en vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import path from "path";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: '/notes-app/',
   plugins: [react()],
   resolve: {
     alias: {
