a828fd37222e0c164db1b5e25d2cf501813f2062
SPallazhco <sergiopall@hotmail.com>
2025-01-28 17:56:23 -0500

    Cambio de endpoint PROD

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://login-service-q30o.onrender.com";
 
 export const API_ENDPOINTS = {
     REFRESH_TOKEN: `${BASE_URL}/api/auth/refresh-token`,
