64279cb36b5e88f39d7706de78d576a6af5c4e2f
SPallazhco <sergiopall@hotmail.com>
2025-01-28 19:16:42 -0500

    Correccion de rutas en git hub

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Navigate, BrowserRouter} from "react-router-dom";
+import {Routes, Route, Navigate, HashRouter} from "react-router-dom";
 import LoginLayout from "./screens/Login/LoginLayout.jsx";
 import NotesLayout from "./screens/notes/NotesLayout.jsx";
 import ProtectedRoute from "./routes/ProtectedRoute.jsx";
@@ -7,7 +7,7 @@ import './index.css';
 
 const App = () => {
     return (
-        <BrowserRouter basename="/notes-app">
+        <HashRouter basename="/notes-app">
             <Routes>
                 {/* Ruta para el login */}
                 <Route path="/login" element={<LoginLayout />} />
@@ -25,7 +25,7 @@ const App = () => {
                 {/* Ruta por defecto en caso de una URL no encontrada */}
                 <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
